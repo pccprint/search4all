64b1ac28610426b1cc78085fefbdf15a1a2645dd
fatwang2 <wangding0798@gmail.com>
2024-03-18 02:26:20 +0800

    更新统计代码

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,11 +1,32 @@
 "use client";
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { Footer } from "@/app/components/footer";
 import { Logo } from "@/app/components/logo";
 import { PresetQuery } from "@/app/components/preset-query";
 import { Search } from "@/app/components/search";
-import React from "react";
+
+declare global {
+  interface Window { gtag: any; }
+}
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      window.gtag('config', process.env.NEXT_PUBLIC_GA_ID, {
+        page_path: url,
+      });
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <div className="absolute inset-0 min-h-[500px] flex items-center justify-center">
       <div className="relative flex flex-col gap-8 px-4 -mt-24">
